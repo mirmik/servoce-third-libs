a26aa6357d8d25fab4265a77e437ae8ac4a7306a
netricks <netricks@protonmail.com>
2020-03-11 00:10:01 +0300

    osxlibs

---
+++ scripts/collect_osx.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+listlibs = [
+	"TKernel",
+	"TKMath",
+	"TKG2d",
+	"TKG3d",
+	"TKBRep",
+	"TKGeomBase",
+	"TKGeomAlgo",
+	"TKTopAlgo",
+	"TKPrim",
+	"TKBO",
+	"TKBool",
+	"TKOffset",
+	"TKService",
+	"TKV3d",
+	"TKOpenGl",
+	"TKFillet",
+	"TKSTL",
+	"TKBin",
+	"TKShHealing",
+	"TKMesh",
+	"TKHLR",
+
+	#darwin
+	"TKCAF",
+	"TKLCAF",
+	"TKCDF",
+	"TKBinL",
+]
+
+for l in listlibs:
+	src = "/usr/local/lib/" + "lib" + l + ".dylib"
+	tgt = "./osx/" + "lib" + l + ".dylib"
+	os.system(f"cp {src} {tgt}")
